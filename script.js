a36db8ea3a704406aaf0cd835fa58eacc2c29442
AmanKarn-code <amankarn.hzb1@gmail.com>
2024-08-15 12:14:10 +0530

    stylling added

--- script.js
+++ script.js
@@ -15,6 +15,20 @@
     document.querySelector('.loader').style.display = 'none';
 });
 
+// nabar sticky 
+// Select the navbar
+const navbar = document.querySelector('.nav-container');
+
+window.addEventListener('scroll', function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        navbar.style.boxShadow = "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px";
+    } else {
+        navbar.style.boxShadow = "none";
+    }
+});
+
+
+
 
 // back to top
 // Get the button
