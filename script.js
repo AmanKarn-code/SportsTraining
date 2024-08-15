23883e21d7c225a45c2ea2ef207fff6418da2954
AmanKarn-code <amankarn.hzb1@gmail.com>
2024-08-15 10:30:02 +0530

    back to to

--- script.js
+++ script.js
@@ -14,3 +14,29 @@
  window.addEventListener('load', function() {
     document.querySelector('.loader').style.display = 'none';
 });
+
+
+// back to top
+// Get the button
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
